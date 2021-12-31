987838835fac2ca4d5a95971ec304b43e5aa49fd
Robert Higdon <robert@ulastnight.com>
2021-12-31 11:42:26 -0800

    Adding slack bolt

--- src/functions/comment-handler.js
+++ src/functions/comment-handler.js
@@ -1,6 +1,18 @@
 'use strict';
 
 var request = require("request");
+var bolt = require('@slack/bolt');
+
+const expressReceiver = new bolt.ExpressReceiver({
+  signingSecret: `${process.env.SLACK_SIGNING_SECRET}`,
+  processBeforeResponse: true
+});
+
+const app = new bolt.App({
+  signingSecret: `${process.env.SLACK_SIGNING_SECRET}`,
+  token: `${process.env.SLACK_BOT_TOKEN}`,
+  receiver: expressReceiver
+});
 
 // populate environment variables locally.
 require('dotenv').config()
