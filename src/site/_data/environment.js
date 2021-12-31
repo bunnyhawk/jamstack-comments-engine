5da395d49daf7be82e88e9fbbeac9044078ee96b
Robert Higdon <robert@ulastnight.com>
2021-12-30 20:14:07 -0800

    Testing env changes

--- src/site/_data/environment.js
+++ src/site/_data/environment.js
@@ -1,6 +1,6 @@
 // Make it easier for us to provide some UI logic
 // based on whether environment variables have been initialised
-
+require('dotenv').config();
 module.exports = () => {
 
   const {
@@ -10,9 +10,9 @@ module.exports = () => {
   } = process.env;
 
   return {
-    ready : NETLIFY_AUTH_TOKEN && SLACK_WEBHOOK_URL ? true : false,
-    NETLIFY_AUTH_TOKEN_ready : NETLIFY_AUTH_TOKEN ? true : false,
-    SLACK_WEBHOOK_URL_ready : SLACK_WEBHOOK_URL ? true : false,
+    ready: true, // NETLIFY_AUTH_TOKEN && SLACK_WEBHOOK_URL ? true : false,
+    NETLIFY_AUTH_TOKEN_ready: NETLIFY_AUTH_TOKEN ? true : false,
+    SLACK_WEBHOOK_URL_ready: SLACK_WEBHOOK_URL ? true : false,
     SITE_NAME
   }
 
